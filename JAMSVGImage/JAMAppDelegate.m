b47de651e8c81d0417d194ac019935c1bdb27c84
alexeyc <alexey.chirkov@rubylight.com>
2020-06-03 11:24:30 +0300

    testing

--- JAMSVGImage/JAMAppDelegate.m
+++ JAMSVGImage/JAMAppDelegate.m
@@ -1,13 +1,13 @@
 
 #import "JAMAppDelegate.h"
-#import "JAMViewController.h"
+#import "JAMDemoViewController.h"
 
 @implementation JAMAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [UIWindow.alloc initWithFrame:UIScreen.mainScreen.bounds];
-    self.window.rootViewController = JAMViewController.new;
+    self.window.rootViewController = JAMDemoViewController.new;
     [self.window makeKeyAndVisible];
     return YES;
 }
---
+++ JAMSVGImage/JAMDemoViewController.h
@@ -0,0 +1,18 @@
+//
+//  JAMDemoViewController.h
+//  JAMSVGImage
+//
+//  Created by Alexey Chirkov on 02/06/2020.
+//  Copyright © 2020 Jeff Menter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@import UIKit;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JAMDemoViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
